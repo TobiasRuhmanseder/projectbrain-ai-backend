81a041fbcaca13f9a82bf74e29a3a03845f483ab
Tobias Ruhmanseder <tobi.r1707@gmail.com>
2026-09-11 14:18:49 +0200

    chore: initialize ProjectBrain AI backend

---
+++ src/main/java/de/projectbrain/backend/ProjectBrainAiApplication.java
@@ -0,0 +1,13 @@
+package de.projectbrain.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBrainAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBrainAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/projectbrain/backend/ProjectBrainAiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.projectbrain.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBrainAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
