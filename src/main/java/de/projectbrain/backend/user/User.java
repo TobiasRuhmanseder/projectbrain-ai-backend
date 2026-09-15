5feb7d9d258bd0531c7b1be9ba8112c70fe739df
Tobias Ruhmanseder <tobi.r1707@gmail.com>
2026-09-15 21:51:08 +0200

    Add Flyway user schema and JPA validation

--- src/main/java/de/projectbrain/backend/user/User.java
+++ src/main/java/de/projectbrain/backend/user/User.java
@@ -1,6 +1,7 @@
 package de.projectbrain.backend.user;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
@@ -12,6 +13,7 @@ import java.time.Instant;
 import java.util.UUID;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
